b2bb619de1a3dba248daadedc018af38e41590d0
sherzod0723 <sherzodsobirov2307@gmail.com>
2024-01-30 18:37:54 +0500

    first committ

--- course/models.py
+++ course/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from parler.models import TranslatableModel, TranslatedFields
 from django.utils.translation import gettext as _
+        
+
+class Teacher(TranslatableModel):
+    course_category = models.ForeignKey('CourseCategory', on_delete=models.PROTECT)
+    translations = TranslatedFields(
+        full_name=models.CharField(_('FUll_Name'), max_length=250)
+    )
+    rating = models.PositiveSmallIntegerField()
+    views = models.IntegerField()
+    student_count = models.IntegerField()
+    course_count = models.IntegerField()
+    about_teacher = models.TextField()
+    facebook = models.URLField()
+    instagram = models.URLField()
+    telegram = models.URLField()
+    linkendin = models.URLField()
 
 
 class AboutCourse(TranslatableModel):
@@ -25,14 +41,31 @@ class CourseCategory(TranslatableModel):
     def __str__(self):
         return self.name
 
+
 class Course(TranslatableModel):
+    category = models.ForeignKey(CourseCategory, on_delete=models.PROTECT)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
     tarnslations = TranslatedFields(
-        category=models.ForeignKey(CourseCategory, on_delete=models.PROTECT),
         title=models.CharField(_('Title'), max_length=250),
         short_body=RichTextField(_('Short body')),
         body=RichTextField(_('Body')),
         price=models.CharField(_('Price'), max_length=250),
-        duration=models.CharField(_('Duration'), max_length=250)
+        duration=models.CharField(_('Duration'), max_length=250),
+        requemnts=RichTextField(_('Requements')),
+        description=RichTextField(_('Description'))
     )
     photo = models.ImageField(upload_to='course/')
     student_count = models.IntegerField(_('Student count'), default=0)
+    language = models.CharField('Language', max_length=50)
+    created_at = models.DateTimeField('Created at', auto_now_add=True)
+    updated_at = models.DateTimeField('Updated at', auto_now=True)
+    certified = models.BooleanField('Certified', default=True)
+    rating = models.PositiveSmallIntegerField('Rating', default=1)
+
+
+class CourseComments(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    body = models.TextField()
+
+
+
